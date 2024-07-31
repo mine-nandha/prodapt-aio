fe05db56ef251d8c2a2c6df20f10bed5a7a04cb1
Nandha Kishore S <nandhakishore.s@prodapt.com>
2024-07-31 16:37:54 +0100

    fix: TypeError: Cannot read properties of undefined (reading 'value')

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,53 +17,55 @@ import Link from "next/link";
 export default function NavbarComponent() {
   const cookieStore = cookies();
   return (
-    <Navbar fluid rounded>
-      <NavbarBrand href={process.env.BASE_URL}>
-        <ProdaptLogo className="w-1/4" />
-      </NavbarBrand>
-      <div className="flex md:order-2">
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar>
-              <AvatarImage />
-              <AvatarFallback>
-                {cookieStore.get("fullName").value.charAt(0)}
-              </AvatarFallback>
-            </Avatar>
-          }
-        >
-          <DropdownHeader>
-            <span className="block text-sm">
-              {cookieStore.get("fullName").value}
-            </span>
-            <span className="block truncate text-sm font-medium">
-              {cookieStore.get("email").value}
-            </span>
-          </DropdownHeader>
-          <DropdownItem>Dashboard</DropdownItem>
-          <DropdownItem>Settings</DropdownItem>
-          <DropdownItem>Earnings</DropdownItem>
-          <DropdownDivider />
-          <DropdownItem>Sign out</DropdownItem>
-        </Dropdown>
-        <NavbarToggle />
-      </div>
-      <NavbarCollapse>
-        <Link href={process.env.BASE_URL}>
-          <NavbarLink as="div">Home</NavbarLink>
-        </Link>
-        <Link href={process.env.BASE_URL + "/profile"}>
-          <NavbarLink as="div">Profile</NavbarLink>
-        </Link>
-        <Link href={process.env.BASE_URL + "/timesheet"}>
-          <NavbarLink as="div">Timesheet</NavbarLink>
-        </Link>
-        <Link href={process.env.BASE_URL + "/foodticket"}>
-          <NavbarLink as="div">Food Ticket</NavbarLink>
-        </Link>
-      </NavbarCollapse>
-    </Navbar>
+    cookieStore?.get("fullName") && (
+      <Navbar fluid rounded>
+        <NavbarBrand href={process.env.BASE_URL}>
+          <ProdaptLogo className="w-1/4" />
+        </NavbarBrand>
+        <div className="flex md:order-2">
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar>
+                <AvatarImage />
+                <AvatarFallback>
+                  {cookieStore.get("fullName").value.charAt(0)}
+                </AvatarFallback>
+              </Avatar>
+            }
+          >
+            <DropdownHeader>
+              <span className="block text-sm">
+                {cookieStore.get("fullName").value}
+              </span>
+              <span className="block truncate text-sm font-medium">
+                {cookieStore.get("email").value}
+              </span>
+            </DropdownHeader>
+            <DropdownItem>Dashboard</DropdownItem>
+            <DropdownItem>Settings</DropdownItem>
+            <DropdownItem>Earnings</DropdownItem>
+            <DropdownDivider />
+            <DropdownItem>Sign out</DropdownItem>
+          </Dropdown>
+          <NavbarToggle />
+        </div>
+        <NavbarCollapse>
+          <Link href={process.env.BASE_URL}>
+            <NavbarLink as="div">Home</NavbarLink>
+          </Link>
+          <Link href={process.env.BASE_URL + "/profile"}>
+            <NavbarLink as="div">Profile</NavbarLink>
+          </Link>
+          <Link href={process.env.BASE_URL + "/timesheet"}>
+            <NavbarLink as="div">Timesheet</NavbarLink>
+          </Link>
+          <Link href={process.env.BASE_URL + "/foodticket"}>
+            <NavbarLink as="div">Food Ticket</NavbarLink>
+          </Link>
+        </NavbarCollapse>
+      </Navbar>
+    )
   );
 }
